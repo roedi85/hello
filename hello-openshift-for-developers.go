942d8c5d28b1c276326b668cc9cd78791eaf85e9
roedi85 <46707154+roedi85@users.noreply.github.com>
2023-06-20 14:49:35 +0700

    rhomi version Update hello-openshift-for-developers.go
    rhomi version Update hello-openshift-for-developers.go

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello wold untuk anda rhoma"
+		response = "Hello wold untuk anda rhomi"
 	}
 
 	fmt.Fprintln(w, response)
