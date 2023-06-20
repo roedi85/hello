16855c9f4a04d4fef039df4dd8eefbdda50322b4
roedi85 <46707154+roedi85@users.noreply.github.com>
2023-06-20 14:56:33 +0700

    semua Update hello-openshift-for-developers.go

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello wold untuk anda rhomi"
+		response = "Hello wold untuk anda anda ssemua"
 	}
 
 	fmt.Fprintln(w, response)
